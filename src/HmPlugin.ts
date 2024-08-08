f9597fbd9c6f49607b6d14a7d08038fdc7d85aba
정회민 <wony1102@naver.com>
2024-08-08 14:32:52 +0900

    feat(HmPagination): Pagination 컴포넌트 추가

--- src/HmPlugin.ts
+++ src/HmPlugin.ts
@@ -2,12 +2,15 @@ import type { App } from 'vue';
 import '@/shared/css.hm-component.scss';
 import { HmTable, HmTableColumn } from '@/components';
 import { type ColumnInfoType } from '@/components/HmTable/type.hm-table';
+import HmPagination from '@/components/HmPagination/HmPagination.vue';
 
 export default {
 	install: (app: App) => {
 		app.component('HmTable', HmTable);
 		app.component('HmTableColumn', HmTableColumn);
+		app.component('HmPagination', HmPagination);
 	},
 };
 
-export { HmTable, HmTableColumn, ColumnInfoType };
+export { HmTable, HmTableColumn, HmPagination };
+export type { ColumnInfoType };
