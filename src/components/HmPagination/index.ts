52664ecb1faacefab4f895d6102c6225dadbce33
정회민 <wony1102@naver.com>
2024-08-09 10:38:07 +0900

    fix: 배포를 위한 구조 수정

---
+++ src/components/HmPagination/index.ts
@@ -0,0 +1,1 @@
+export { default as HmPagination } from './HmPaginationComponent.vue';
--- src/hm-plugin/index.ts
+++ src/hm-plugin/index.ts
@@ -1,8 +1,8 @@
-import type { App } from 'vue';
 import '@/shared/css.hm-component.scss';
-import { HmTable, HmTableColumn } from '@/components';
-import { type ColumnInfoType } from '@/components/HmTable/type.hm-table';
-import HmPagination from '@/components/HmPagination/HmPagination.vue';
+import type { App } from 'vue';
+
+import { HmTable, HmTableColumn, HmPagination } from '@/components';
+import type { ColumnInfoType } from '@/components';
 
 export default {
 	install: (app: App) => {
